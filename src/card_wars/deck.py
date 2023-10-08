5cf71b1caf14cc3e047b84a381ad86e18d5c877f
William L <41084946+helveticae@users.noreply.github.com>
2023-10-08 11:14:02 +0200

    improved fill_with_card, add_card, add remove_card

--- src/card_wars/deck.py
+++ src/card_wars/deck.py
@@ -15,16 +15,18 @@ class Deck:
     cards: List[Card] = field(default_factory=list)
 
     def add_card(self, card: Card):
-        assert isinstance(
-            card, Card
-        ), "'card' argument must be an instance of the 'Card' class. Did you enter a string?"
+        """
+        Add a copy of any card to deck card list.
+        """
+        if isinstance(card, str):
+            card = find_card(card)
 
-        if len(self.cards) < self.card_limit:
-            # Create a new instance of the card and add it to the deck
-            new_card = copy.deepcopy(card)
-            self.cards.append(new_card)
-        else:
-            print(f"Deck is full ({self.card_limit} cards). Cannot add more cards.")
+        if card is not None:
+            if len(self.cards) < self.card_limit:
+                new_card = copy.deepcopy(card)
+                self.cards.append(new_card)
+            else:
+                print(f"Deck is full ({self.card_limit} cards). Cannot add more cards.")
 
     def shuffle(self):
         if self.cards == []:
@@ -41,15 +43,25 @@ class Deck:
         else:
             print("Deck is empty. Cannot draw a card.")
 
-    def fill_with_card(self, card: Card):
+    def fill_with_card(self, card: Card, fill: float = 1.0):
         """
         Fill the deck with a specific card until the deck limit is reached.
         """
+        if fill <= 0.0:
+            return
 
-        while len(self.cards) < self.card_limit:
-            # Create a new instance of the card and add it to the deck
+        max_deck = int(self.card_limit * fill)
+
+        if max_deck > self.card_limit:
+            max_deck = self.card_limit
+
+        cards_to_add = max_deck - len(self.cards)
+
+        for _ in range(cards_to_add):
             new_card = copy.deepcopy(card)
-            self.cards.append(new_card)
+            self.add_card(new_card)
+
+        print(f"Added {cards_to_add} copies of {card}")
 
     def get_card(self, index: int) -> Card:
         """
@@ -67,11 +79,35 @@ class Deck:
         """
         self.cards = []
 
+    def remove_card(self, card_input):
+        """
+        Remove all copies of a specified card from the card list based on user input.
+        """
+        if isinstance(card_input, str):
+            card = find_card(card_input)
+        elif isinstance(card_input, Card):
+            card = card_input
+        else:
+            print("Invalid input. Please enter a card name or provide a Card object.")
+            return
+
+        if card is not None:
+            self.cards = [c for c in self.cards if c.card_id != card.card_id]
+            print(f"Removed all copies of '{card}' from the deck.")
+
     def __str__(self):
-        deck_str = f"{self.name} - {len(self.cards)} cards:\n"
+        deck_str = f"{self.name} - [{len(self.cards)}/{self.card_limit}] cards:\n"
+
         for card in self.cards:
-            deck_str += f"  {card.card_id, card.name}\n"
+            if card is not None:
+                deck_str += f"  {card.card_id, card.name}\n"
+            else:
+                deck_str += "  (Empty)\n"
+
         return deck_str
+
+    def __len__(self):
+        return len(self.cards)
 
 
 def get_test_deck(deck_type: str = "goblin") -> Deck:
@@ -100,4 +136,17 @@ if __name__ == "__main__":
     random_deck = get_test_deck("random")
     print(random_deck)
     goblin_deck.burn_deck()
+    goblin_deck.add_card("Dragonite3")
+    print(goblin_deck)
+    print(len(goblin_deck))
+    goblin_deck.burn_deck()
+    print(len(goblin_deck))
     goblin_deck.add_card("asdf")
+    print(len(goblin_deck))
+    goblin_deck.add_card("Gnome")
+    goblin_deck.fill_with_card("Grandma Gnome", fill=-1.5)
+    goblin_deck.fill_with_card("Grandma Gnome", fill=0.5)
+    goblin_deck.fill_with_card(find_card("Grandma Gnome"), fill=9.9)
+    print(len(goblin_deck))
+    goblin_deck.remove_card(find_card("Grandma Gnome"))
+    print(goblin_deck)
