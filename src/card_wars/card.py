4a0e66e9c6ada5b9b2113b07b728092f4f095c63
William L <41084946+helveticae@users.noreply.github.com>
2023-10-20 12:52:05 +0200

    update minion health logic

--- src/card_wars/card.py
+++ src/card_wars/card.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Tuple
 
 from card_wars.logs import logger
 
@@ -11,70 +11,82 @@ class Card:
     card_id: str
     name: str
     description: str
-    mana_cost: int
     card_text: str
+    mana_cost: int = 0
 
 
 @dataclass
 class Minion(Card):
-    attack: int
-    max_health: int
-    health: int = None
-    race: str = None
+    base_stats: Tuple[int, int, int] = (0, 0, 0)
+    attack: int = 0
+    health: List[int] = field(default_factory=lambda: [0, 0])
     ability: List[dict] = field(default_factory=list)
     battlecry: List[dict] = field(default_factory=list)
     deathrattle: List[dict] = field(default_factory=list)
+    race: str = None
 
     def __post_init__(self):
-        if self.health is None:
-            self.health = self.max_health
+        self.attack = self.base_stats[0]
+        self.health[0] = self.base_stats[1]  # Current health
+        self.health[1] = self.base_stats[1]  # Max health
+        self.mana_cost = self.base_stats[2]
+
+    def get_minion_attack_value(self):
+        # TODO Check board buffs:
+        # For example "When a friendly Goblin attacks, give it +3 attack"
+        buff_value = 0
+
+        return self.attack + buff_value
 
     def take_damage(self, damage):
-        if "divine_shield" in self.ability:
+        # // keep this check here until more damage affecting abilities added
+
+        if "divine_shield" in self.ability and damage > 0:
             self.ability.remove("divine_shield")
-            log(f"{self.name} took no damage but lost divine shield.")
+            log(f"{self.name} lost Divine Shield.")
             return
 
+        ## //
+
         if damage > 0:
-            self.health -= damage
-            log(f"{self.name} took {damage} damage: [{self.attack}/{self.health}]")
+            self.health[0] -= damage
+            log(f"{self.name} took {damage} damage: [{self.attack}/{self.health[0]}]")
         else:
             print(f"Invalid damage value: {damage}")
 
     def attack_target(self, target):
         if self.attack <= 0:
-            print(f"Cannot attack with {self.name} (attack value <= 0).")
+            print(f"{self.name} cannot attack with {self.attack=}")
             return
 
         elif isinstance(target, Minion):
             log(
-                f"{self.name} [{self.attack}/{self.health}] attacks {target.name} [{target.attack}/{target.health}] for {self.attack} damage."
+                f"{self.name} [{self.get_minion_attack_value()}/{self.health[0]}] attacks {target.name} [{target.attack}/{target.health[0]}] for {self.get_minion_attack_value()} damage."
             )
-            target.take_damage(self.attack)
-            self.take_damage(target.attack)
+
+            target.take_damage(self.get_minion_attack_value())
+            self.take_damage(target.get_minion_attack_value())
         else:
-            target.take_damage(self.attack)
+            target.take_damage(self.get_minion_attack_value())
             log(f"{self.name} attacked {target.name} for {self.attack} damage.")
 
     def heal(self, value):
-        self.health = min(self.health + value, self.max_health)
+        self.health[0] = min(self.health[0] + value, self.health[1])
 
     def __str__(self):
-        str = (
-            f"{self.name}: [{self.attack}/{self.health}] battlecry={self.battlecry}, {self.ability}"
-        )
+        str = f"{self.name}: [{self.attack}/{self.health[0]}] {self.mana_cost=} {self.battlecry=}, {self.ability=}"
         return str
 
 
 @dataclass
 class Spell(Card):
-    spell_type: str
-    target: int  # 0 = all entities, 1 = enemy entities, 2 = enemy minion only, 3 = player minion only, 4 = player character only etc (?)
+    spell_type: str = "Default"
+    target: int = 0  # 0 = all entities, 1 = enemy entities, 2 = enemy minion only, 3 = player minion only, 4 = player character only etc (?)
     damage: int = 0
 
 
 @dataclass
 class Weapon(Card):
-    attack: int
-    durability: int
+    attack: int = 0
+    durability: int = 0
     ability: str = None
