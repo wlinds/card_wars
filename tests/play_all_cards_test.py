edff7b09df234045bf945e3169a94fbd720341dc
William L <41084946+helveticae@users.noreply.github.com>
2023-11-05 15:05:46 +0100

    minions, weapons and spells

--- tests/play_all_cards_test.py
+++ tests/play_all_cards_test.py
@@ -7,14 +7,14 @@ from card_wars.player import Player
 
 
 @logs.game_state
-def test_all_minion():
+def test_all_cards():
     p1 = Player(name="P1 ALL", deck=Deck(), max_hand_size=999)
     p2 = Player(name="P2 ALL", deck=Deck(), max_hand_size=999)
 
     board = Board()
     board.max_field_minion = 999
 
-    all_minions = get_all_cards(minions=True, weapons=0, spells=0)
+    all_minions = get_all_cards(minions=True, weapons=True, spells=True)
 
     for minion in all_minions:
         p1.deck.add_card(minion)
@@ -22,14 +22,11 @@ def test_all_minion():
 
     cw = GameSession(p1, p2, board)
 
-    cw.player1.active_mana = 999
-    cw.player2.active_mana = 999
-
     for i in range(len(p1.deck) + 1):
-        cw.draw_card(1)
+        p1.draw_card()
         cw.play_card(1, 0)
         cw.play_card(1, 0)
-        cw.draw_card(2)
+        p2.draw_card()
         cw.play_card(2, 0)
         cw.play_card(2, 0)
 
@@ -40,15 +37,33 @@ def test_all_minion():
         for minion in cw.board.p1_field:
             minion.take_damage(1)
             cw.remove_dead_minions(1)
+            cw.player1.active_mana = 9999
 
     while len(cw.board.p2_field) > 0:
         for minion in cw.board.p2_field:
             minion.take_damage(1)
             cw.remove_dead_minions(2)
+            cw.player2.active_mana = 9999
+
+    print(cw)
+    print(cw.board)
+    print(p2.get_hand())
+
+    while len(p2.hand) > 0 and len(p1.hand) > 0:
+        p1.draw_card()
+        p2.draw_card()
+        cw.player2.active_mana = 9999
+        cw.player1.active_mana = 9999
+        cw.play_card(1, 0)
+        cw.play_card(1, -1)
+        cw.play_card(2, 0)
+        cw.play_card(2, -1)
+        cw.remove_dead_minions(1)
+        cw.remove_dead_minions(2)
 
     print(cw)
     print(cw.board)
 
 
 if __name__ == "__main__":
-    test_all_minion()
+    test_all_cards()
