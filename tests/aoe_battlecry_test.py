d98b2df223adeab4e7c221637c4f39ace3fe2f11
William L <41084946+helveticae@users.noreply.github.com>
2023-11-05 15:04:21 +0100

    test aoe dmg and heal

--- tests/aoe_battlecry_test.py
+++ tests/aoe_battlecry_test.py
@@ -27,9 +27,9 @@ class TestAOEBattlecry(unittest.TestCase):
             cw.board.add_to_field(find_card("Gnome"), 2)
 
         # Add dragon to P1 hand, set mana
-        cw.player1_hand.append(find_card("Dragonite"))
+        cw.player1.hand.append(find_card("Dragonite"))
         cw.player1.active_mana = 16
-        print(cw.player1_hand)
+        print(cw.player1.hand)
         print(len(cw.board))
 
         # Crush all goblins except divine shields
@@ -40,7 +40,7 @@ class TestAOEBattlecry(unittest.TestCase):
         print(len(cw.board))
 
         # add second dragon, play it to crush the rest of the grandmas (jesus)
-        cw.player1_hand.append(find_card("Dragonite"))
+        cw.player1.hand.append(find_card("Dragonite"))
         cw.play_card(1, 0)
 
         cw.remove_dead_minions(2)
@@ -48,6 +48,22 @@ class TestAOEBattlecry(unittest.TestCase):
         self.assertEqual(len(cw.board), 2)  # Should only be 2 dragons
         self.assertEqual(board.p2_field, [])  # P2 Board should be cleared
         print(cw.board)
+
+        cw.board.p1_field[0].take_damage(2)
+        cw.board.p1_field[0].take_damage(2)
+        cw.player1.take_damage(1)
+
+        cw.player1.active_mana += 10
+        cw.player2.active_mana += 10
+
+        cw.play_card(2, find_card("mgob006"))
+        cw.play_card(2, find_card("mgob006"))
+        cw.play_card(1, find_card("mgob006"))
+
+        print(board)
+
+        # Should be restored to full health
+        self.assertEqual(cw.board.p1_field[0].health[0], cw.board.p1_field[0].base_stats[1])
 
 
 if __name__ == "__main__":
